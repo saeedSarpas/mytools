50312776b987d68e1bc5bc32ef1411c266e5e2ec
saeedSarpas <s.sarpas@gmail.com>
2016-07-14 00:42:43 +0200

    Separated gadget module into a new directory (simulation)

---
+++ simulation/__init__.py
@@ -0,0 +1,3 @@
+"""Simulation"""
+
+__all__ = ['gadget']
--- simulation/gadget.py
+++ simulation/gadget.py
@@ -1,17 +1,28 @@
-"""gadgetreader.py
-Load Gadget snapshot"""
+"""gadget.py
+Loading Gadget snapshot"""
 
 import struct
 import numpy  as np
 
 class Gadget(object):
     """Tool for loading and manipulating Gadget 2 snapshot.
-    [NOTE] This module only support N-body simulations"""
 
-    def __init__(self, fname):
+    .. note:: This module only support N-body simulations
+
+    Parameters
+    ----------
+    path : str
+        Path to the gadget snapshot
+
+    Examples
+    --------
+    >>> from mytools.simulation.gadget import Gadget
+    >>> gadget = Gadget('/path/to/gadget/snapshot')
+    """
+
+    def __init__(self, path):
         """Initializing"""
-        self.fname = fname
-        self._file = open(fname, 'rb')
+        self._file = open(path, 'rb')
         self.headers = self._readheader()
         self.data = []
         self.dtype = np.dtype([('id', np.int),
@@ -20,16 +31,47 @@ class Gadget(object):
                                ('z', np.float64)])
 
     def load(self):
-        """Load gadget data"""
+        """Load gadget snapshot into a numpy array
+
+        Returns
+        -------
+        numpy.ndarray
+
+        Examples
+        --------
+        >>> gadget.load()
+        """
+
         self.data = self._readdata()
+
         return self.data
 
     def index(self, col='x'):
-        """Sorting the data array for a given column"""
+        """Sorting the data based on a given column
+
+        Parameters
+        ----------
+        x : str
+            the name of the column to be sorted
+
+        Returns
+        -------
+        numpy.ndarray
+
+        Examples
+        --------
+        >>> gadget.index(col='z')
+        """
+
         return np.sort(self.data, order=col)
 
     def _readheader(self):
-        """Extracting gadget header"""
+        """Extracting gadget header
+
+        Returns
+        -------
+        dict
+        """
         headerlen = struct.unpack('i', self._file.read(4))[0] + 4
         fmtstring = "6i8d9i{0:d}x".format(headerlen - 124)
         headers = struct.unpack(fmtstring, self._file.read(headerlen))
@@ -47,16 +89,20 @@ class Gadget(object):
         }
 
     def _readdata(self):
-        """Extracting position data"""
+        """Extracting position data
+
+        Returns
+        -------
+        numpy.ndaaray
+        """
+
         positionslen = struct.unpack('i', self._file.read(4))[0] + 4
         fmtstring = "{0:d}f4x".format(self.headers['nparticles']*3)
         positions = struct.unpack(fmtstring, self._file.read(positionslen))
-        data = np.array(
-            np.zeros((self.headers['nparticles'])),
-            dtype=self.dtype)
+
+        tmp = []
         for i in range(self.headers['nparticles']):
-            data['id'][i] = i
-            data['x'][i] = positions[i*3]
-            data['y'][i] = positions[i*3 + 1]
-            data['z'][i] = positions[i*3 + 2]
-        return data
+            tmp.append(
+                (i, positions[i*3], positions[i*3 + 1], positions[i*3 + 2]))
+
+        return np.array(tmp, dtype=self.dtype)
