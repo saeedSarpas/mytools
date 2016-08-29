1d98a45d2073c6c01cb5d62e4acd4a0d28546610
saeedSarpas <s.sarpas@gmail.com>
2016-08-29 19:20:28 +0200

    Moved rockstar related files under halofinder/rocksta/ directory

--- halofinder/rockstar/load_rockstar_bin.h
+++ halofinder/rockstar/load_rockstar_bin.h
@@ -2,9 +2,9 @@
 #define LOAD_ROCKSTAR_H
 
 
-#include "./../memory/allocate.h"
-#include "./../io/open_file.h"
-#include "./../io/read_from.h"
+#include "./../../memory/allocate.h"
+#include "./../../io/open_file.h"
+#include "./../../io/read_from.h"
 #include "./rockstar_data_type.h"
 
 
