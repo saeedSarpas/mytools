77cd216f88a41f9bc7d7b342c1aa33be81055d47
saeedSarpas <s.sarpas@gmail.com>
2016-09-02 20:42:54 +0200

    Added boundry_check to three_to_one function

--- grid/row_major_order/three_to_one.c
+++ grid/row_major_order/three_to_one.c
@@ -2,8 +2,9 @@
  * grid/row_major_roder/three_to_one.c
  * test_file: grid/row_major_roder/three_to_one_test.c
  *
- * Converting the index of an element in a 3D array to its row-major
- * counter index
+ * Converting the index of an element in a 3D array to its row-major counter
+ * index part. Note that this function assume the box is periodic so index -1
+ * is actually the last grid in the same axis
  *
  * param: i, j, k indices of the element in the 3D array
  * param: dims array containing the dimensions of the box
@@ -17,11 +18,28 @@
 #include <math.h>
 
 
+static int boundry_check(int, int);
+
+
 int three_to_one(int i, int j, int k, int *dims)
 {
-	i = i % dims[0];
-	j = j % dims[1];
-	k = k % dims[2];
+  i = boundry_check(i, dims[0]);
+	j = boundry_check(j, dims[1]);
+	k = boundry_check(k, dims[2]);
 
 	return (i * dims[2] * dims[1]) + (j * dims[2]) + k;
+}
+
+
+static int boundry_check(int pos, int axis_length)
+{
+  if(pos >= 0 && pos <= axis_length - 1)
+    return pos;
+  else
+    if (pos > axis_length - 1)
+      pos %= axis_length;
+    else if (pos < 0)
+      pos += axis_length;
+
+  return boundry_check(pos, axis_length);
 }
--- grid/row_major_order/three_to_one_test.c
+++ grid/row_major_order/three_to_one_test.c
@@ -22,13 +22,39 @@ AfterEach(three_to_one){}
 #define ZLEN 12
 
 
-Ensure(three_to_one, returns_right_results_for_a_trivial_input)
+static int dims[3] = {XLEN, YLEN, ZLEN};
+
+
+Ensure(three_to_one, returns_right_results_for_a_trivial_inputs)
 {
-  int i, j, k = ZLEN / 2, index, dims[3] = {XLEN, YLEN, ZLEN};
+  int i, j, k = ZLEN / 2, index;
 
   for(i = 0; i < 2 * XLEN; i++)
     for(j = 0; j < YLEN; j++){
       index = three_to_one(i, j, k, dims);
       assert_that(index, is_equal_to((i % XLEN) * (YLEN * ZLEN) + j * ZLEN + k));
     }
+}
+
+
+Ensure(three_to_one, returns_right_result_for_negative_inputs)
+{
+  int index;
+  index = three_to_one(-1, 0, 0, dims);
+  assert_that(index, is_equal_to((XLEN - 1) * YLEN * ZLEN));
+
+  index = three_to_one(-1 * XLEN - 1, 0, 0, dims);
+  assert_that(index, is_equal_to((XLEN - 1) * YLEN * ZLEN));
+
+  index = three_to_one(0, -1, 0, dims);
+  assert_that(index, is_equal_to((YLEN - 1) * ZLEN));
+
+  index = three_to_one(0, -1 * YLEN - 1, 0, dims);
+  assert_that(index, is_equal_to((YLEN - 1) * ZLEN));
+
+  index = three_to_one(0, 0, -1, dims);
+  assert_that(index, is_equal_to((ZLEN - 1)));
+
+  index = three_to_one(0, 0, -1 * ZLEN - 1, dims);
+  assert_that(index, is_equal_to((ZLEN - 1)));
 }
