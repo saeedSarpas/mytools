d356151329bd4868659c828361182ef3f92e2748
Saeed Sarpas <saeed@astro.uni-bonn.de>
2016-12-02 03:19:56 +0100

    Added functions to load matching halos from rg_matchfinder output

---
+++ examples/rg_matchfinder/load_mh.h
@@ -0,0 +1,13 @@
+#ifndef LOAD_MH_H
+#define LOAD_MH_H
+
+
+#include "./../../avltree/avl_tree.h"
+#include "./../../halomatcher/singlehalo_matcher.h"
+
+
+avl_node* load_mh(char*);
+int get_num_of_mh(char*);
+
+
+#endif /* LOAD_MH_H */
